50b4f442a1362401d514fbb441c233658b4dfa8f
Vinothini-Saravanan <vinothinibe1306@gmail.com>
2024-10-13 21:43:36 +0530

    Add files via upload
    Datatype home assignment submitted

---
+++ homeassignment/Chrome.java
@@ -0,0 +1,20 @@
+package week1.homeassignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 float a=91.0f;
+		 System.out.println("versionoftype:"+a);
+		 String b="Google";
+		 System.out.println("develper of type:"+b);
+		 boolean c=false;
+		 System.out.println("isBeta of type:"+c);
+		 int d=2008;
+		 System.out.println("releaseYear:"+d);
+		 char e='C';
+		 System.out.println("shortcutKey:"+e);
+
+	}
+
+}
